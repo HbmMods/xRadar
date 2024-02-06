ef7be2097d80393253ce643dcb6177195d9aa9df
Ruslanllll <innaryslanllll@gmail.com>
2024-02-06 18:10:47 +0200

    Wind

--- src/main/java/com/hfr/tileentity/machine/TileEntityMachineWindmill.java
+++ src/main/java/com/hfr/tileentity/machine/TileEntityMachineWindmill.java
@@ -4,54 +4,27 @@ import net.minecraft.block.material.Material;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraftforge.common.util.ForgeDirection;
 
-import com.hfr.blocks.BlockSpeedy;
-import com.hfr.main.MainRegistry;
-import com.hfr.util.Location;
+import com.hbm.tileentity.TileEntityLoadedBase;
+import com.hfr.util.TileUtil;
 
-import cofh.api.energy.EnergyStorage;
-import cofh.api.energy.IEnergyProvider;
-import cofh.api.energy.IEnergyReceiver;
+import api.hbm.energy.IEnergyGenerator;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class TileEntityMachineWindmill extends TileEntity implements IEnergyProvider {
+public class TileEntityMachineWindmill extends TileEntityLoadedBase implements IEnergyGenerator {
 
-    public EnergyStorage storage = new EnergyStorage(
-        MainRegistry.windmillProduction * 10,
-        MainRegistry.windmillProduction,
-        MainRegistry.windmillProduction);
+    final static public long maxPower = 500_000;
+    private long power;
 
     @SideOnly(Side.CLIENT)
     public float rotation;
 
     public void updateEntity() {
 
-        if (!worldObj.isRemote) {
-
-            if (operational()) {
-                storage.receiveEnergy(MainRegistry.windmillProduction, false);
-
-                for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-
-                    if (!canConnectEnergy(dir)) continue;
-
-                    Location loc = new Location(worldObj, xCoord, yCoord, zCoord).add(dir)
-                        .add(dir);
-                    TileEntity entity = loc.getTileEntity();
-
-                    if (entity != null && entity instanceof IEnergyReceiver) {
-
-                        IEnergyReceiver receiver = (IEnergyReceiver) entity;
-
-                        int maxExtract = storage.getMaxExtract();
-                        int maxAvailable = storage.extractEnergy(maxExtract, true);
-                        int energyTransferred = receiver.receiveEnergy(dir.getOpposite(), maxAvailable, false);
-                        storage.extractEnergy(energyTransferred, false);
-                    }
-                }
-            }
+        if (!worldObj.isRemote && operational()) {
+            power += 2_250;
+            if (power > maxPower) power = maxPower;
         }
     }
 
@@ -62,48 +35,25 @@ public class TileEntityMachineWindmill extends TileEntity implements IEnergyProv
                 .getMaterial() != Material.air && !(i == 0 && j == 0)
                 || !worldObj.canBlockSeeTheSky(xCoord + i, yCoord + 32, zCoord + j)) return false;
 
-        for (int x = -1; x <= 1; x++) for (int z = -1; z <= 1; z++)
-            if (!(worldObj.getBlock(xCoord + x, yCoord - 1, zCoord + z) instanceof BlockSpeedy)) return false;
+        if (!TileUtil.hasPlatform(worldObj, xCoord, yCoord, zCoord)) {
+            return false;
+        }
 
         return true;
-    }
-
-    @Override
-    public boolean canConnectEnergy(ForgeDirection from) {
-        return from != ForgeDirection.UP && from != ForgeDirection.DOWN && from != ForgeDirection.UNKNOWN;
-    }
-
-    @Override
-    public int extractEnergy(ForgeDirection from, int maxExtract, boolean simulate) {
-        return storage.extractEnergy(maxExtract, simulate);
-    }
-
-    @Override
-    public int getEnergyStored(ForgeDirection from) {
-
-        if (canConnectEnergy(from)) return storage.getEnergyStored();
-        return 0;
-    }
-
-    @Override
-    public int getMaxEnergyStored(ForgeDirection from) {
-
-        if (canConnectEnergy(from)) return storage.getMaxEnergyStored();
-        return 0;
     }
 
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
         super.readFromNBT(nbt);
 
-        storage.readFromNBT(nbt);
+        power = nbt.getLong("power");
     }
 
     @Override
     public void writeToNBT(NBTTagCompound nbt) {
         super.writeToNBT(nbt);
 
-        storage.writeToNBT(nbt);
+        nbt.setLong("power", power);
     }
 
     @Override
@@ -116,4 +66,20 @@ public class TileEntityMachineWindmill extends TileEntity implements IEnergyProv
     public double getMaxRenderDistanceSquared() {
         return 65536.0D;
     }
+
+    @Override
+    public void setPower(long arg0) {
+        power = arg0;
+    }
+
+    @Override
+    public long getMaxPower() {
+        return maxPower;
+    }
+
+    @Override
+    public long getPower() {
+        return power;
+    }
+
 }
